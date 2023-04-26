98a2a1b6e4e0fcf5b475c4272ab6935b68947a47
ryiephry <ephraimpt2@gmail.com>
2023-04-26 11:36:57 -0400

    3
    i dont know why append isnt working
    , and for some reason i cant grab the li tag

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,5 +3,24 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+const galleryImageEL = document.getElementsByClassName("gallery__item");
+
+console.log(galleryImageEL);
+
+let newimgEL = " ";
+
+for (let i = 0; i < galleryItems.length; i++){
+    console.log(galleryItems[i]);
+
+    let previewImageEL = galleryItems[i];
+    
+    newimgEL = document.createElement("img");
+    
+    newimgEL.innerHTML += `class="gallery__image" src = "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg" data-source=${galleryItems[i]} alt="Image description" />`;
+   galleryImageEL.append(newimgEL);
+};
+
+
+
 
 
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,5 +2,3 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
-
-console.log("hi")
